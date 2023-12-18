804d4051012aba4799844b34ecf98960afeea51b
mashiklavashik <409162@niuitmo.ru>
2023-12-18 18:52:08 +0300

    Update: unit_test_square.py tests fixed

--- tests/unit/unit_test_square.py
+++ tests/unit/unit_test_square.py
@@ -5,16 +5,31 @@ from square import area, perimeter
 
 
 class SquareTest(unittest.TestCase):
-    def test_area(self):
+    def test_area_int(self):
         self.assertAlmostEqual(area(3), 9)
-        self.assertAlmostEqual(area(-99), 9801)
+
+    def test_area_negative(self):
+        with self.assertRaises(Exception):
+            area(-99)
+
+    def test_area_float(self):
         self.assertAlmostEqual(area(16.77), 281.2329)
+
+    def test_area_0(self):
         self.assertAlmostEqual(area(0), 0)
 
-    def test_perimeter(self):
+    def test_perimeter_int(self):
         self.assertAlmostEqual(perimeter(3), 12)
-        self.assertAlmostEqual(perimeter(-99), -396)
+
+    def test_perimeter_negative(self):
+        with self.assertRaises(Exception):
+            perimeter(-99)
+
+
+    def test_perimeter_float(self):
         self.assertAlmostEqual(perimeter(16.77), 67.08)
+
+    def test_perimeter_0(self):
         self.assertAlmostEqual(perimeter(0), 0)
 
 if __name__ == '__main__':
