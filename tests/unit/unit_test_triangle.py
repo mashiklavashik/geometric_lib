7714d8a0d09707b2c1266e1450001df4ae7684cb
mashiklavashik <409162@niuitmo.ru>
2023-12-18 18:44:40 +0300

    Update: unit_test_triangle.py tests fixed

--- tests/unit/unit_test_triangle.py
+++ tests/unit/unit_test_triangle.py
@@ -5,16 +5,31 @@ from triangle import area, perimeter
 
 
 class TriangleTest(unittest.TestCase):
-    def test_area(self):
+    def test_area_int(self):
         self.assertAlmostEqual(area(3, 6), 9)
-        self.assertAlmostEqual(area(-7, 12), -42)
-        self.assertAlmostEqual(area(3.5, 6.8), 11.9)
-        self.assertAlmostEqual(area(12.1, 8), 48.4)
 
-    def test_perimeter(self):
+    def test_area_negative(self):
+        with self.assertRaises(Exception):
+            area(-7, 12)
+
+    def test_area_float(self):
+        self.assertAlmostEqual(area(3.5, 6.8), 11.9)
+
+    def area_test_0(self):
+        self.assertAlmostEqual(area(12.1, 0), 0)
+
+    def test_perimeter_int(self):
         self.assertAlmostEqual(perimeter(3, 4, 5), 12)
-        self.assertAlmostEqual(perimeter(-12, 4, 8), 0)
-        self.assertAlmostEqual(perimeter(0, 98, 0), 98)
+
+    def test_perimeter_negative(self):
+        with self.assertRaises(Exception):
+            perimeter(-12, 4, 8)
+
+    def test_perimeter_0(self):
+        with self.assertRaises(Exception):
+            perimeter(0, 98, 0)
+
+    def test_perimeter_float(self):
         self.assertAlmostEqual(perimeter(9.766, 16, 9.01),34.775999999999996 )
 
 if __name__ == '__main__':
