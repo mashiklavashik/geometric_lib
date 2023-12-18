ad495763879eb33bc3f9a6c96037264e36775147
mashiklavashik <409162@niuitmo.ru>
2023-12-18 18:19:20 +0300

    Update: unit_test_circle.py tests fixed

--- tests/unit/unit_test_circle.py
+++ tests/unit/unit_test_circle.py
@@ -1,21 +1,34 @@
 import unittest
-
 import circle
 from circle import area, perimeter
 
-
 class CircleTest(unittest.TestCase):
-    def test_area(self):
+    def test_area_int(self):
         self.assertAlmostEqual(area(8), 201.06192982974676)
+
+    def test_area_float(self):
         self.assertAlmostEqual(area(9.31), 272.30099900181426)
-        self.assertAlmostEqual(area(-15), 706.8583470577034)
+    def test_area_0(self):
         self.assertAlmostEqual(area(0), 0)
 
-    def test_perimeter(self):
+    def test_area_negative(self):
+        with self.assertRaises(Exception):
+            area(-15)
+
+
+
+    def test_perimeter_int(self):
         self.assertAlmostEqual(perimeter(8), 50.26548245743669)
+
+    def test_perimter_float(self):
         self.assertAlmostEqual(perimeter(9.31), 58.49645520984195)
-        self.assertAlmostEqual(perimeter(-15),-94.24777960769379 )
+
+    def test_perimeter_0(self):
         self.assertAlmostEqual(perimeter(0), 0)
+
+    def test_perimeter_negative(self):
+        with self.assertRaises(Exception):
+            perimeter(-15)
 
 
 if __name__ == '__main__':
